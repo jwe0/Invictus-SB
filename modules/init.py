3a44580226128f5d5bb50ef14c8bc59f87830a79
jwe0 <jwe0@epic.x>
2024-07-17 20:37:16 +0100

    Updated Demo custom style

--- modules/init.py
+++ modules/init.py
@@ -97,7 +97,8 @@ async def example(ctx):
                     "CMDEnd": "`",
                     "Split": "|",
                     "Footer": "```test```",
-                    "Header": "```{}```"
+                    "Header": "```{}```",
+                    "Align" : False
                 }
             }
 
